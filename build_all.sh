9a273240a4029d7d11354ca54f39715e9112bd96
lucadani7 <79052990+lucadani7@users.noreply.github.com>
2026-01-18 16:13:36 +0200

    Enhance `build_all.sh` script: specify project file in `dotnet publish` commands, add auto-compression of output directories into `.zip` archives, and clean intermediate folders.

--- build_all.sh
+++ build_all.sh
@@ -9,12 +9,21 @@ rm -rf $OUTPUT_DIR
 dotnet clean "${PROJECT_NAME}.csproj" -c Release
 mkdir -p $OUTPUT_DIR
 echo "Generating executable for Windows (win-x64)..."
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o "$OUTPUT_DIR/Windows"
+dotnet publish "${PROJECT_NAME}.csproj" -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o "$OUTPUT_DIR/Windows"
 echo "Generating executable for Linux (linux-x64)..."
-dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -o "$OUTPUT_DIR/Linux"
+dotnet publish "${PROJECT_NAME}.csproj" -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -o "$OUTPUT_DIR/Linux"
 echo "Generating executable for Mac Apple Silicon (osx-arm64)..."
-dotnet publish -c Release -r osx-arm64 --self-contained true -p:PublishSingleFile=true -o "$OUTPUT_DIR/Mac_Silicon"
+dotnet publish "${PROJECT_NAME}.csproj" -c Release -r osx-arm64 --self-contained true -p:PublishSingleFile=true -o "$OUTPUT_DIR/Mac_Silicon"
 echo "Generating executable for Mac Intel (osx-x64)..."
-dotnet publish -c Release -r osx-x64 --self-contained true -p:PublishSingleFile=true -o "$OUTPUT_DIR/Mac_Intel"
+dotnet publish "${PROJECT_NAME}.csproj" -c Release -r osx-x64 --self-contained true -p:PublishSingleFile=true -o "$OUTPUT_DIR/Mac_Intel"
+echo "Auto-compressing in .zip archives..."
+cd "$OUTPUT_DIR" || { echo "Error: Couldn't enter into $OUTPUT_DIR"; exit 1; }
+zip -r Windows.zip Windows/ > /dev/null
+zip -r Linux.zip Linux/ > /dev/null
+zip -r Mac_Silicon.zip Mac_Silicon/ > /dev/null
+zip -r Mac_Intel.zip Mac_Intel/ > /dev/null
+echo "Cleaning intermediar folders (Windows/, Mac/, etc.)..."
+rm -rf Windows/ Linux/ Mac_Silicon/ Mac_Intel/
+cd .. || { echo "Error: Couldn't return to root directory"; exit 1; }
 echo "--- PROCESS IS OVER! ---"
-echo "All the executables are in this folder: $OUTPUT_DIR"
+echo "In this folder you can find .zip archives: $OUTPUT_DIR"
